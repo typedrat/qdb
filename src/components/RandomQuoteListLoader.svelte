d185ca42da8de7cc2feaffd8b744c58e2dbe4404
Alexis Williams <alexis@typedr.at>
2024-03-25 19:09:36 -0700

    Test a fully static site.

---
+++ src/components/RandomQuoteListLoader.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Reservoir from "reservoir";
+    import Quote, { quoteSchema } from "./Quote.svelte";
+
+    export let ids: number[] = [];
+    export let len = 25;
+
+    const randomIds = Reservoir<number>(len);
+    randomIds.pushSome(...ids);
+
+    const quotesPromise = Promise.all(
+        randomIds.map(async (id) => {
+            const res = await fetch(`/quote/${id}.json`);
+            return quoteSchema.parse(await res.json());
+        })
+    );
+</script>
+
+{#await quotesPromise}
+    <p>Loading random quotes...</p>
+{:then quotes}
+    {#each quotes as quote}
+        <Quote data={quote} />
+    {/each}
+{/await}
