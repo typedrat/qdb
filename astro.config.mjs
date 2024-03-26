7342c70d31585cfbede384f2a186bfdd5c92e43a
Alexis Williams <alexis@typedr.at>
2024-03-25 20:04:27 -0700

    Enable view transitions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), svelte()],
-  output: "static"
+    integrations: [icon(), svelte()],
+    output: "static",
+    prefetch: { prefetchAll: true },
 });
